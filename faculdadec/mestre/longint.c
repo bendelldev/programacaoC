04ff224a042fd6e2c2bfb758bd00567775da9ed6
Bender Dev <167443361+bendelldev@users.noreply.github.com>
2025-11-17 16:53:33 +0000

    yes

---
+++ faculdadec/mestre/longint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int numeroNormal = 2147483647; // Valor máximo de int
+    long int numeroGrande = 2147483647;
+
+    printf("Numero regular (int): %d\n", numeroNormal);
+    printf("Numero grande (long int): %ld\n", numeroGrande);
+
+    numeroGrande = 2147483648; // Valor maior que o máximo de int
+    printf("Numero grande atualizado (long int): %ld\n", numeroGrande);
+
+
+
+
+}
